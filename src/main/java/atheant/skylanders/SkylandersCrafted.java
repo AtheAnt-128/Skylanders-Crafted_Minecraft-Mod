7058db674c7fa10d577ec35e7cedb6b07b902fa1
AtheAnt <younggun0625@gmail.com>
2025-09-12 09:10:19 -0400

    Initial commit

---
+++ src/main/java/atheant/skylanders/SkylandersCrafted.java
@@ -0,0 +1,16 @@
+package atheant.skylanders;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SkylandersCrafted implements ModInitializer {
+	public static final String MOD_ID = "skylanders";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/atheant/skylanders/SkylandersCraftedClient.java
@@ -0,0 +1,10 @@
+package atheant.skylanders;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkylandersCraftedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
